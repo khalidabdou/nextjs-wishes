3a169a30ff87f4d7b1274485e8ecc455a55f16d6
abdellah khalid <37934938+khalidabdou@users.noreply.github.com>
2022-08-18 08:23:05 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,70 +1,65 @@
-
 import Header from "./comp/Header";
 import SEO from "./comp/SEO";
 import Section from "./comp/Section";
-import useTranslation from 'next-translate/useTranslation'
+import useTranslation from "next-translate/useTranslation";
 import Reviews from "./comp/reviews";
 
 export default function Home() {
+  const { t, lang } = useTranslation("common");
 
+  const imageTitle = t("imageTitle");
+  const imageDescription = t("imageDescription");
+  const show = t("show");
 
-  const { t, lang } = useTranslation('common')
-  
-  const imageTitle = t('imageTitle')
-  const imageDescription = t('imageDescription')
-  const show = t('show')
+  const stickerTitle = t("stickerTitle");
+  const stickerDescription = t("stickerDescription");
 
-  const stickerTitle = t('stickerTitle')
-  const stickerDescription = t('stickerDescription')
+  const wallpaperTitle = t("wallpaperTitle");
+  const wallpaperDescription = t("wallpaperDescription");
 
-  const wallpaperTitle = t('wallpaperTitle')
-  const wallpaperDescription = t('wallpaperDescription')
-
-  const quoteTitle = t('quoteTitle')
-  const quoteDescription = t('quoteDescription')
-  
+  const quoteTitle = t("quoteTitle");
+  const quoteDescription = t("quoteDescription");
 
   return (
     <div>
-      <SEO/>
+      <SEO />
       <main>
         <Header />
         <Section
-          id={'images'}
+          id={"images"}
           title={imageTitle}
           description={imageDescription}
           button={show}
-          backgound={'bg-white'}
+          backgound={"bg-white"}
           image={"/img.jpg"}
           dir={""}
         />
-         <Section
-         id={'stickers'}
+        <Section
+          id={"stickers"}
           title={stickerTitle}
           description={stickerDescription}
           button={show}
-          
           image={"/stickers.jpg"}
           dir={"rtl"}
         />
-         <Section
-         id={'quotes'}
+        <Section
+          id={"quotes"}
           title={quoteTitle}
           description={quoteDescription}
           button={show}
-          backgound={'bg-white'}
+          backgound={"bg-white"}
           image={"/quotes.jpg"}
           dir={""}
         />
-         <Section
-         id={'wallpapers'}
+        <Section
+          id={"wallpapers"}
           title={wallpaperTitle}
           description={wallpaperDescription}
           button={show}
           image={"/wallpapers.jpg"}
           dir={"rtl"}
         />
-      <Reviews /> 
+        <Reviews />
       </main>
     </div>
   );
